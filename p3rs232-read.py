0d5c7f867a778b7717d7111acaf96b31ebe452ec
EmilianoA-B <emi.ariza.b@gmail.com>
2024-11-26 21:00:50 -0600

    Final touches

--- p3rs232-read.py
+++ p3rs232-read.py
@@ -1,7 +1,14 @@
 import serial 
 from datetime import datetime 
 try:
-    portA = serial.Serial('COM9')
+    portA = serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate=9600,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        rtscts=True
+        )
     fecha = datetime.now()
     fecha = "Fecha de recepcion: " + fecha.strftime("%d/%m/%Y - %H:%M:%S")
     archivo = open("LogP3-remoto.txt","a")
@@ -14,6 +21,8 @@ try:
         message = portA.readline().decode()
         print(">>> " + message, end="")
         archivo.write("\tListened: " + message)
+        portA.reset_input_buffer()
+        portA.reset_output_buffer()
     archivo.write("\n---------Finalizacion de transimicion---------\n")
 
 except Exception:
